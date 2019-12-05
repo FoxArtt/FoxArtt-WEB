4259410c4549582e09cac1e32e15c28f4d09d3e3
Edycar Reyes <edycarreyes@gmail.com>
2019-12-04 22:56:18 -0600

    Se crean los archivos para la vista del sitio 'Under Construction'

---
+++ src/pages/UnderConstruction/UnderConstruction.stories.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import UnderConstruction from './UnderConstruction';
+
+storiesOf('UnderConstruction', module).add('default', () => <UnderConstruction />);
---
+++ src/pages/UnderConstruction/UnderConstruction.tsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './UnderConstruction.scss';
+
+type props = {};
+type state = {};
+
+class UnderConstruction extends Component<props, state> {
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+    }
+
+    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+        return (
+            <div className="UnderConstruction" data-testid="UnderConstruction">
+                UnderConstruction Component
+            </div>
+        );
+    }
+}
+
+export default UnderConstruction;
