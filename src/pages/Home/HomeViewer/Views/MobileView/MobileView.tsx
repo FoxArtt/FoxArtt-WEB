04b35cc962745f10eb61d14f06f3bd8cf814eb6c
jonariv09 <ingjoseagustinnarvaez@gmail.com>
2019-12-08 15:26:40 -0600

    Creando estructura de la seccion media de la pagina

--- src/pages/Home/HomeViewer/Views/MobileView/MobileView.tsx
+++ src/pages/Home/HomeViewer/Views/MobileView/MobileView.tsx
@@ -1,65 +1,86 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
 // Styles imports
-import './MobileView.scss';
+import './MobileView.scss'
 
 // Components imports
-import HamburgerMenu from './HamburgerMenu';
+import HamburgerMenu from './HamburgerMenu'
 
 // Frames imports
-import {Col, Container, Row} from "reactstrap";
+import { Col, Container, Row } from 'reactstrap'
 
 // Images imports
-import MainBannerImage from "../../../../../assets/images/Home/backgroundHomeLandingCreate.svg";
-import logoHeader from '../../../../../assets/images/Logos/FoxArtt-Logo-WEB.svg'
-
+import logoHeader
+  from '../../../../../assets/images/Logos/FoxArtt-Logo-WEB.svg'
+import middleImage
+  from '../../../../../assets/images/Home/middleSectionImage.jpg'
 
 type props = {};
 type state = {};
 
 class MobileView extends Component<props, state> {
-    constructor(props: props) {
-        super(props);
-        this.state = {};
-    }
+  constructor (props: props) {
+    super(props)
+    this.state = {}
+  }
 
-    render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-        return (
-            <div className="MobileView" data-testid="MobileView">
-                <Container>
+  render (): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+    return (
+      <div className="MobileView" data-testid="MobileView">
+        <Container>
 
-                    {/* Logo and HamburgerMenu */}
-                    <Row className="d-flex justify-content-center">
-                        <Col xs={{size: 11}} className="mt-4">
-                            <Row className="d-flex justify-content-between">
-                                <img src={logoHeader} className="LogoHeader"/>
-                                <HamburgerMenu/>
-                            </Row>
-                        </Col>
-                    </Row>
-                    {/* Logo and HamburgerMenu */}
-                </Container>
+          {/* Logo and HamburgerMenu */}
+          <Row className="d-flex justify-content-center">
+            <Col xs={{ size: 11 }} className="mt-4">
+              <Row className="d-flex justify-content-between">
+                <img src={logoHeader} className="LogoHeader"/>
+                <HamburgerMenu/>
+              </Row>
+            </Col>
+          </Row>
+          {/* Logo and HamburgerMenu */}
+        </Container>
 
-                <Container>
+        <Container>
 
-                    <Row className="d-flex justify-content-center">
+          <Row className="d-flex justify-content-center">
 
 
-                        <Col xs={{size: 11}} className="mt-1 MainBannerImage">
-                        </Col>
+            <Col xs={{ size: 11 }} className="mt-1 MainBannerImage">
+            </Col>
 
-                        <Col xs={{size: 11}} className="IntroductoryText my-4 text-left">
-                            <h3 className="IntroductoryTitle"> FoxArtt </h3>
-                            <p className="IntroductoryParagraph mt-2"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos eveniet nam nisi quo rem suscipit tempore velit. Alias, obcaecati, quisquam. </p>
-                            
-                        </Col>
-                    </Row>
+            <Col xs={{ size: 11 }} className="IntroductoryText my-4 text-left">
+              <h3 className="IntroductoryTitle"> FoxArtt </h3>
+              <p className="IntroductoryParagraph mt-2"> Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Dignissimos eveniet nam nisi
+                quo rem suscipit tempore velit. Alias, obcaecati, quisquam. </p>
+              <a href="#" className="buttonLearnMore"> Learn More </a>
+            </Col>
+          </Row>
+        </Container>
 
-                </Container>
 
+        <Container className="middleSection">
+          <Container fluid className="ContainerMiddleSection">
+            <div className="backgroundContainer">
+              <img src={middleImage} width="100%"
+                   className="ImageContainerMiddle"/>
+
+              <div className="InformativeMiddleSection">
+
+              </div>
+
+              <div>
+              </div>
             </div>
-        );
-    }
+
+          </Container>
+            
+        </Container>
+
+      </div>
+    )
+  }
 }
 
-export default MobileView;
+export default MobileView
